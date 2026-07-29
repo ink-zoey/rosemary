ade566fc95b77ae040a3481c868ba8415672c11f
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-07-28 18:10:32 -0700

    the rejiter

---
+++ src/Rosemary/Common/TEMP_ChatManagerFix.cs
@@ -0,0 +1,25 @@
+﻿using Daybreak.Hooks;
+using Terraria.UI.Chat;
+
+namespace Rosemary.Common;
+
+// TODO: Remove when Daybreak.ChatTags fixes this.
+file static class TEMP_ChatManagerFix
+{
+    [OnLoad]
+    private static void Load()
+    {
+        IL_ChatManager.DrawColorCodedStringShadow_SpriteBatch_DynamicSpriteFont_IEnumerable1_Vector2_Color_float_Vector2_Vector2_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringShadow_SpriteBatch_DynamicSpriteFont_List1_Vector2_Color_float_Vector2_Vector2_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringShadow_SpriteBatch_DynamicSpriteFont_string_Vector2_Color_float_Vector2_Vector2_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringWithShadow_SpriteBatch_DynamicSpriteFont_TextSnippetArray_Vector2_Color_Color_float_Vector2_Vector2_refInt32_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringWithShadow_SpriteBatch_DynamicSpriteFont_TextSnippetArray_Vector2_Color_float_Vector2_Vector2_refInt32_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringWithShadow_SpriteBatch_DynamicSpriteFont_TextSnippetArray_Vector2_float_Vector2_Vector2_refInt32_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringWithShadow_SpriteBatch_DynamicSpriteFont_string_Vector2_Color_Color_float_Vector2_Vector2_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedStringWithShadow_SpriteBatch_DynamicSpriteFont_string_Vector2_Color_float_Vector2_Vector2_float_float += _ => { };
+        IL_ChatManager.DrawColorCodedString_SpriteBatch_DynamicSpriteFont_IEnumerable1_Vector2_Color_float_Vector2_Vector2_refInt32_float_bool += _ => { };
+        IL_ChatManager.DrawColorCodedString_SpriteBatch_DynamicSpriteFont_IEnumerable1_Vector2_float_Vector2_Vector2_refInt32_Nullable1 += _ => { };
+        IL_ChatManager.DrawColorCodedString_SpriteBatch_DynamicSpriteFont_IEnumerable1_Vector2_float_Vector2_Vector2_refInt32_float += _ => { };
+        IL_ChatManager.DrawColorCodedString_SpriteBatch_DynamicSpriteFont_string_Vector2_Color_float_Vector2_Vector2_float_bool += _ => { };
+    }
+}
