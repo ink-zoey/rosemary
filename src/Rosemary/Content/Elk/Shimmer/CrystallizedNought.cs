d5bf04842f1f3594c8c71070a2b63b1831f9e17c
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-09-05 20:03:49 -0700

    nought wip

---
+++ src/Rosemary/Content/Elk/Shimmer/CrystallizedNought.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Rosemary.Content.Elk;
+
+public sealed class CrystallizedNought : ModItem
+{
+    public override string Texture => Assets.Elk.TestItem.KEY;
+
+    public override string LocalizationCategory => "Content.Elk";
+
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 20;
+
+        ItemID.Sets.SolidShimmerReaction[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 14;
+        Item.height = 26;
+        Item.maxStack = Item.CommonMaxStack;
+
+        Item.rare = ItemRarityID.Purple;
+
+        Item.value = Item.buyPrice(gold: 3);
+    }
+}
--- src/Rosemary/Content/Elk/Shimmer/ItemSets.cs
+++ src/Rosemary/Content/Elk/Shimmer/ItemSets.cs
@@ -32,12 +32,15 @@ public static class ElkShimmerItemSets
 
     private static bool[] violentShimmerReaction = [];
 
+    private static bool[] solidShimmerReaction = [];
+
     private static Mod Mod => ModContent.GetInstance<ModImpl>();
 
     [ModSystemHooks.ResizeArrays]
     private static void ResizeArrays()
     {
         violentShimmerReaction = CreateSet(nameof(violentShimmerReaction), false);
+        solidShimmerReaction = CreateSet(nameof(solidShimmerReaction), false);
 
         return;
 
@@ -55,8 +58,11 @@ public static class ElkShimmerItemSets
         ///     Made for use with <see cref="IViolentShimmerReactant"/> applied on the <see cref="ModItem"/>.
         /// </summary>
         public static bool[] ViolentShimmerReaction => violentShimmerReaction;
+
+        public static bool[] SolidShimmerReaction => solidShimmerReaction;
     }
 
+#region Violent
     [OnLoad]
     private static void Load_ViolentShimmerReaction()
     {
@@ -597,7 +603,8 @@ public static class ElkShimmerItemSets
             attenuationDistance: 5500f
         );
 
-        if (curPosition.Distance(Main.screenPosition + new Vector2(Main.screenWidth, Main.screenHeight) * 0.5f) > 3300f)
+        if (!subSurface
+         && curPosition.Distance(Main.screenPosition + new Vector2(Main.screenWidth, Main.screenHeight) * 0.5f) > 3300f)
         {
             SoundEngine.PlaySound(
                 Assets.Elk.Shimmer.EjectionFar.Asset with
@@ -613,6 +620,11 @@ public static class ElkShimmerItemSets
         if (reactant?.Ejection(self, subSurface) is true)
         {
             self.ClearOut();
+        }
+
+        if (subSurface)
+        {
+            return;
         }
 
         const int center_width = 80;
@@ -979,4 +991,96 @@ public static class ElkShimmerItemSets
 
         return curPosition;
     }
+#endregion
+
+#region Solid
+    [OnLoad]
+    private static void Load_SolidShimmerReaction()
+    {
+        On_WorldItem.ApplyMovement += ApplyMovement_ShimmerWalk;
+
+        IL_WorldItem.MoveInWorld += _ => { };
+        IL_WorldItem.UpdateItem += _ => { };
+    }
+
+    private static void ApplyMovement_ShimmerWalk(On_WorldItem.orig_ApplyMovement orig, WorldItem self, ref Vector2 wetVelocity)
+    {
+        var velocity = self.wet ? wetVelocity : self.velocity;
+
+        if (ItemID.Sets.SolidShimmerReaction[self.type] && !self.wet)
+        {
+            var prior = velocity;
+
+            velocity = ShimmerCollision(self, velocity);
+
+            if (velocity != prior)
+            {
+                self.velocity.X /= 0.95f;
+                self.velocity.X *= 0.99f;
+            }
+        }
+
+        self.position += velocity;
+
+        return;
+
+        static Vector2 ShimmerCollision(Entity item, Vector2 velocity)
+        {
+            var result = velocity;
+            var nextPosition = item.position + velocity;
+            var position = item.position;
+
+            var startX = (int)(item.position.X / 16f) - 1;
+            var endX = (int)((item.position.X + item.width) / 16f) + 2;
+            var startY = (int)(item.position.Y / 16f) - 1;
+            var endY = (int)((item.position.Y + item.height) / 16f) + 2;
+
+            startX = Utils.Clamp(startX, 0, Main.maxTilesX - 1);
+            endX = Utils.Clamp(endX, 0, Main.maxTilesX - 1);
+            startY = Utils.Clamp(startY, 0, Main.maxTilesY - 40);
+            endY = Utils.Clamp(endY, 0, Main.maxTilesY - 40);
+
+            var current = Vector2.Zero;
+
+            for (var i = startX; i < endX; i++)
+            for (var j = startY; j < endY; j++)
+            {
+                var tile = Main.tile[i, j];
+
+                if (!tile.HasShimmer || Main.tile[i, j - 1].HasShimmer)
+                {
+                    continue;
+                }
+
+                var level = 16f - (((Main.tile[i, j].LiquidAmount / (float)byte.MaxValue) * 16f) + 2);
+
+                current.X = i * 16f;
+                current.Y = j * 16f + level;
+
+                if (nextPosition.X + item.width > current.X
+                 && nextPosition.X < current.X + 16f
+                 && nextPosition.Y + item.height > current.Y
+                 && nextPosition.Y < current.Y + level
+                 && position.Y + item.height <= current.Y)
+                {
+                    result.Y = current.Y - (position.Y + item.height);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    private static void MoveInWorld_ShimmerWalk(ILContext il)
+    {
+        return;
+
+        
+    }
+
+
+
+
+
+#endregion
 }
